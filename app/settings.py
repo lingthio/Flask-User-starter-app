2f9530ff07a754ef805b23358bb40307b9da6fe7
Pascua Theus <pascua@theus.name>
2018-09-24 18:56:04 +0200

    Removed unused import 'os'

--- app/settings.py
+++ app/settings.py
@@ -2,8 +2,6 @@
 # Place environment specific settings in env_settings.py
 # An example file (env_settings_example.py) can be used as a starting point
 
-import os
-
 # Application settings
 APP_NAME = "Flask-User starter app"
 APP_SYSTEM_ERROR_SUBJECT_LINE = APP_NAME + " system error"
