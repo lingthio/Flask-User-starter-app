eec12ab964e0c4e73b19be69d41f58e98a845929
liob <hbwinther@metalabs.de>
2020-02-13 16:52:49 +0100

    initial docker support

--- app/local_settings_example.py
+++ app/local_settings_example.py
@@ -12,6 +12,9 @@ DEBUG = True
 #     python -c "import os; print repr(os.urandom(24));"
 SECRET_KEY = 'This is an UNSECURE Secret. CHANGE THIS for production environments.'
 
+# path to data directory for images and segmentations
+DATA_PATH = 'data'
+
 # SQLAlchemy settings
 SQLALCHEMY_DATABASE_URI = 'sqlite:///../app.sqlite'
 SQLALCHEMY_TRACK_MODIFICATIONS = False    # Avoids a SQLAlchemy Warning
---
+++ wsgi.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from app import create_app
+
+application = create_app()
